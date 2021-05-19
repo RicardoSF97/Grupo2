a66304606e1913f1043b50e457b4a09d03ffb9b6
RicardoSF97 <83984759+RicardoSF97@users.noreply.github.com>
2021-05-19 10:55:01 -0500

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,14 @@
+<script>
+$(document).ready(function() {
+ $('btn_add').click(funtion){
+     agregar();
+ });   	
+});
+  var cont=0;
+funtion agregar(){
+    cont++;
+    var fila='<tr class="selected" id="fila'+cont+'"><td>'+cont+'</td 
+    
+    
+}
+</script>
